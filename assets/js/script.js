91364c83b957346f037f0723c5d01fbd790ed618
Tater <haleymariehilton@gmail.com>
2022-04-10 16:42:40 -0700

    Corresponding const applied using document.getElementById() in JS and set-up CSS ids.

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+const container = document.getElementById("container");
+const startBtn = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
+const choices = document.getElementById("choices");
+const submitBtn = document.getElementById("submit");
+const results = document.getElementById("results");
+const highScore = document.getElementById("highScore");
+const timer = document.getElementById("timer");
+const counter = document.getElementById("counter");
+const highScoreContainer = document.getElementById("highScoreContainer");
+
+
+//countdown timer
+
+
+
+//start quiz container
+    //Start button should begin the game
+
+
+//multiple choice questions
+    // keep track of user's answers
+    //on click show results
+        // if answer is correct
+        // add to number of correct answers
+        // display Correct!
+        // if answer is wrong
+        // subtract from number of correct answers
+        // 
+
+
+
+//high score storage
